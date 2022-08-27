ca5c86aa35c916ad99cafc5f6facee3d70466710
Vlad Harkusha <sivari888@gmail.com>
2022-08-27 18:21:31 +0300

    add methods post put delete

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { postAPI } from '../services/postService';
 import { PostItem } from './PostItem';
+import { IPost } from '../models/IPost';
+
 
 export const PostContainer: React.FC = () => {
   const [limit, setLimit] = useState(10)
   const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit, {
     pollingInterval: 1000
   });
+  const [deletePost, { }] = postAPI.useDeletePostMutation();
+  const [updatePost, { }] = postAPI.useUpdatePostMutation();
 
   useEffect(() => {
     setTimeout(() => {
       setLimit(3)
     }, 2000)
   }, [])
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
+  };
 
   return (
     <div>
@@ -22,7 +34,7 @@ export const PostContainer: React.FC = () => {
         {isLoading && <h1>Posts are loading...</h1>}
 
         {posts && posts.map(post => 
-          <PostItem key={post.id} post={post}/>
+          <PostItem key={post.id} post={post} remove={handleRemove} update={handleUpdate} />
         )}
 
         {error && <h1>Loading error!</h1>}
--- src/components/PostContainer2.tsx
+++ src/components/PostContainer2.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import { postAPI } from '../services/postService';
 import { PostItem } from './PostItem';
+import { IPost } from '../models/IPost';
 
 export const PostContainer2: React.FC = () => {
-  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10);
+  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(100);
+  const [createPost, { error: createError, isLoading: isCreateLoading }] = postAPI.useCreatePostMutation();
+  const [deletePost, {}] = postAPI.useDeletePostMutation();
+  const [updatePost, {}] = postAPI.useUpdatePostMutation();
+
+  const handleCreate = async () => {
+    const title = prompt();
+    await createPost({ title, body: title } as IPost)
+  };
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
+  };
 
   return (
     <div>
       <div className="post__list">
+        <button onClick={handleCreate}>ADD_POST</button>
+
         {isLoading && <h1>Posts are loading...</h1>}
 
         {posts && posts.map(post => 
-          <PostItem key={post.id} post={post}/>
+          <PostItem key={post.id} post={post} remove={handleRemove} update={handleUpdate} />
         )}
 
         {error && <h1>Loading error!</h1>}
